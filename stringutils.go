a1378b9edcd225878ad1f38617bb8837a2a98616
Jorgen Nilsson <me@jorgennilsson.com>
2023-09-26 09:49:42 -0700

    Initial commit

---
+++ stringutils.go
@@ -0,0 +1,11 @@
+package stringutils
+
+import "strings"
+
+func Upper(value string) string {
+	return strings.ToUpper(value)
+}
+
+func Lower(value string) string {
+	return strings.ToLower(value)
+}
